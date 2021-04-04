e6092b2fe1e8e2d73c47d4ed58ee199debb8bcdf
braianxde <braianwegmann@hotmail.com>
2021-04-03 23:29:34 -0300

    feito

---
+++ projetointegrador3a/projetointegrador3a/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\braia\Documents\Projetos\projetointegrador3a\projetointegrador3a\projetointegrador3a\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
